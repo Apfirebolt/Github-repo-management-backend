949180f01cb907a8061e3593bcc6a0cc0ad6c41c
APFirebolt <aspper20@gmail.com>
2019-12-11 00:31:17 -0800

    API created for forum

--- forum/urls.py
+++ forum/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.forum_home, name='forum'),
     path('create/', views.CreateThread.as_view(), name='create-thread'),
     path('list/', views.ThreadList.as_view(), name='list-thread'),
-    # path('api/', include(('accounts.api.urls', 'accounts.api'), namespace='accounts-api')),
+    path('api/', include(('forum.api.urls', 'forum.api'), namespace='forum-api')),
 ]
